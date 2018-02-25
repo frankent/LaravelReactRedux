36f284bff74c66e228b71838e286b62233f30995
Kiattirat Sujjapongse <kiattirat@sujjapongse.com>
2018-02-25 15:53:33 +0700

    react first build passed

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
+// let path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +12,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.react('resources/assets/js/app.js', 'public/js').version();
